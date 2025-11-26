0735dce5cef292d53afa22e3d9becf458ff98c7f
Gaeun <gge91@naver.com>
2025-11-26 10:49:17 +0900

    다시

--- src/main/java/com/smu/householdaccount/repository/ItemRepository.java
+++ src/main/java/com/smu/householdaccount/repository/ItemRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+
     // 1. 상품 단건 조회 ( 상품 하나 불러오기 => 결제 페이지에서 상세 정보 띄울 때 사용 )
     Optional<Item> findById(Long id);
 
