a7313cc977409becfa048f5f4fa12a2227fba8f1
Gaeun <gge91@naver.com>
2025-12-01 18:11:17 +0900

    [CHORE] MyContoller, indexhome, mypage 를 일반유저마이페이지의 정보를 불러오기 위해 수정

--- src/main/java/com/smu/householdaccount/controller/MyContoller.java
+++ src/main/java/com/smu/householdaccount/controller/MyContoller.java
@@ -2,17 +2,20 @@ package com.smu.householdaccount.controller;
 
 import com.smu.householdaccount.entity.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-@RequestMapping("/my")
+@RequestMapping("/mypage")
 public class MyContoller {
     @GetMapping
     public String myPage(
-            @SessionAttribute("loginUser") Member loginUser
+            @SessionAttribute("loginUser") Member loginUser,
+            Model model
     ){
+        model.addAttribute("member",loginUser);
         return "user/mypage";
     }
 }
