688896252b8b8130db8fd410cda99464c3829a93
song_youjin <you12zin34@gmail.com>
2025-12-02 14:34:52 +0900

    [FEAT] AWS S3 서버 연결
    - AWS S3 버킷 생성
    - AWS CloudFront 배포 생성
    - 이미지 업로드 구현

--- src/main/java/com/smu/householdaccount/util/Utility.java
+++ src/main/java/com/smu/householdaccount/util/Utility.java
@@ -1,16 +1,18 @@
 package com.smu.householdaccount.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 
+@Component
 public class Utility {
-    // 생성자 방지
-    private Utility(){}
+    @Value("${cloudfront.domain}")
+    private String cloudfrontDomain;
 
     /**
      * 암호화 메서드.Argon2 사용.
@@ -32,5 +34,12 @@ public class Utility {
         YearMonth yearMonth = YearMonth.of(year, month);
         LocalDateTime endDate = yearMonth.atEndOfMonth().atStartOfDay();
         return endDate.getDayOfMonth();
+    }
+
+    public String getPublicUrl(String key) {
+        if (key.startsWith("/")) {
+            key = key.substring(1);
+        }
+        return "https://" + cloudfrontDomain + "/" + key;
     }
 }
