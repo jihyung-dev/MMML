355f8b9912a566afbe9a9de48ef1410590666de1
Gaeun <gge91@naver.com>
2025-12-08 17:41:35 +0900

    [CHORE] 로그아웃해도 세션 남아있어서 로그아웃 진행이 안 돼서 AutoLoginIterceptorDev 주석 처리함

--- src/main/java/com/smu/householdaccount/interceptor/AutoLoginInterceptorDev.java
+++ src/main/java/com/smu/householdaccount/interceptor/AutoLoginInterceptorDev.java
@@ -20,22 +20,22 @@ public class AutoLoginInterceptorDev implements HandlerInterceptor {
     private final SellerService sellerService;
 
     //컨드롤러 도작전에 자동 셀러,유저 로그인
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1,123-01-00006,U006,식품제조/농산물,2025-11-25 10:57:29,
-        HttpSession session = request.getSession();
-        Object loginUser = session.getAttribute("loginUser");
-        if(loginUser!=null) return true;
-
-//        Member loginMember=memberService.login("U006","12345");
-        Member loginMember=memberService.login("U016","1234");
-        session.setAttribute("loginUser",loginMember);
-        session.setAttribute("loginUserId", loginMember.getMemberId());
-
-        // ⭐ 여기 추가: 이 회원이 판매자인지 여부 세션에 저장
-        boolean isSeller = sellerService.getSellerByMemberId(loginMember.getMemberId()) != null;
-        session.setAttribute("isSeller", isSeller);
-
-        return true;
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        //1,123-01-00006,U006,식품제조/농산물,2025-11-25 10:57:29,
+//        HttpSession session = request.getSession();
+//        Object loginUser = session.getAttribute("loginUser");
+//        if(loginUser!=null) return true;
+//
+////        Member loginMember=memberService.login("U006","12345");
+//        Member loginMember=memberService.login("U016","1234");
+//        session.setAttribute("loginUser",loginMember);
+//        session.setAttribute("loginUserId", loginMember.getMemberId());
+//
+//        // ⭐ 여기 추가: 이 회원이 판매자인지 여부 세션에 저장
+//        boolean isSeller = sellerService.getSellerByMemberId(loginMember.getMemberId()) != null;
+//        session.setAttribute("isSeller", isSeller);
+//
+//        return true;
+//    }
 }
